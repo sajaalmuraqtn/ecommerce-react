4f99f2e78efc0abd1ee47d61f128385df5a76c99
sajaalmuraqtn <sajaalmuraqtn@gmail.com>
2023-06-06 02:16:03 +0300

    register and login

--- src/App.js
+++ src/App.js
@@ -1,30 +1,56 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
 import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css';
 import './App.css';
-import Navbar from './Components/Navbar/Navbar';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Layout from './Components/Layout/Layout';
+import Home from './Components/Home/Home';
+import Product from './Components/Product/Product';
+import Register from './Components/Register/Register';
+import Notfound from './Components/NotFound/Notfound';
+import Login from './Components/Login/Login';
+import Cart from './Components/Cart/Cart';
+import jwt from 'jwt-decode';
 
-function App() {
-  return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <Navbar/>
-    </div>
-  );
+export default function App() {
+  
+  let [user,setUser]=useState(null);
+  function saveCurrentUser() {
+    let token=localStorage.getItem('userToken');
+    let decoded=jwt(token);
+    setUser(decoded);
+  }
+  
+function logout() {
+  
 }
 
-export default App;
+  useEffect(
+  ()=>{
+      if (localStorage.getItem("userToken")) {
+        saveCurrentUser();
+      }
+    }
+    ,[]
+  )
+
+  let router = createBrowserRouter(
+    [
+      {path: '', element: <Layout user={user} />, 
+      children: [
+        { index: true, element: <Home /> },
+        { path: 'product', element: <Product/> },
+        { path: 'cart', element: <Cart/> },
+        { path: 'login', element: <Login info={saveCurrentUser}/> },
+        { path: 'register', element: <Register/> },
+        { path: '*', element: <Notfound /> }
+      ]
+    }
+    ]);
+  return (
+<RouterProvider router={router}>
+
+</RouterProvider>  )
+}
+
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,45 +1,52 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-export default class Navbar extends Component {
-  render() {
-    return (
-      <><nav className="navbar navbar-expand-lg bg-primary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon" />
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Links</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li><hr className="dropdown-divider" /></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled">Disabled</a>
-        </li>
-      </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-danger " type="submit">Search</button>
-      </form>
+export default function Navbar({user}) {
+  return (
+    <><nav className="navbar navbar-expand-lg bg-primary">
+    <div className="container-fluid">
+      <a className="navbar-brand" href="#">T-Shop</a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse d-flex justify-content-around" id="navbarSupportedContent">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <Link className="nav-link active" aria-current="page" to="">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link active" aria-current="page" to="product">Product</Link>
+          </li>
+          {
+            user?
+          <li className="nav-item">
+            <Link className="nav-link active" aria-current="page" to="cart">Cart</Link>
+          </li>
+          :
+          ''
+          }
+        </ul>
+
+        <ul className=' navbar-nav ms-auto mb-2' id='navbarSupportedContent'>
+          {
+            !user ?
+            <>
+          <li className="nav-item">
+            <Link className="nav-link active " aria-current="page" to="login">Login</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link active " aria-current="page" to="register">Register</Link>
+          </li>
+            
+            </>:
+          <li className="nav-item">
+            <Link className="nav-link active " aria-current="page" to="login">Logout</Link>
+          </li>
+
+          }
+        </ul>
+      </div>
     </div>
-  </div>
-</nav>
-</>
-    )
-  }
+  </nav>
+  </>  )
 }
